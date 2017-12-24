83121e466dddcc35a1b9e0b9bfed82622f4bcf87
Daniel Strano <stranoj@gmail.com>
2017-12-23 22:15:17 -0500

    Add/Subtract ops

--- example.cpp
+++ example.cpp
@@ -12,29 +12,29 @@ int main() {
 
 	int i, j;
 
-	Qrack::Register qftReg(8, 3);
+	Qrack::Register qftReg(8, 0);
 
 	double qftProbs[8];
-	std::cout<<"ASL Test:"<<std::endl;
+	std::cout<<"UADD Test:"<<std::endl;
 	for (i = 0; i < 8; i++) {
 		for (j = 0; j < 8; j++) {
 			std::cout<<qftReg.Prob(j);
 		}
 		std::cout<<"->";
-		qftReg.ASL(2);
+		qftReg.UADD(1);
 		for (j = 0; j < 8; j++) {
 			std::cout<<qftReg.Prob(j);
 		}
 		std::cout<<std::endl;	
 	}
-	std::cout<<"ASR Test:"<<std::endl;
-	qftReg.SetPermutation(129);
+	std::cout<<"USUB Test:"<<std::endl;
+	qftReg.SetPermutation(128);
 	for (i = 0; i < 8; i++) {
 		for (j = 0; j < 8; j++) {
 			std::cout<<qftReg.Prob(j);
 		}
 		std::cout<<"->";
-		qftReg.ASR(2);
+		qftReg.USUB(1);
 		for (j = 0; j < 8; j++) {
 			std::cout<<qftReg.Prob(j);
 		}
--- qrack.hpp
+++ qrack.hpp
@@ -13,6 +13,7 @@
 
 #include <stdint.h>
 #include <math.h>
+#include <algorithm>
 #include <complex>
 #include <random>
 #include <stdexcept>
@@ -553,6 +554,36 @@ namespace Qrack {
 
 			//Single register instructions:
 
+			///"Circular rotate left" permutation
+			/** "Circular rotate left" permutation - Adds argument to permutation state, carrying highest permutations to lowest. */
+			void RADD(bitCapInt toAdd) {
+				std::rotate(stateVec, stateVec + maxQPower - toAdd, stateVec + maxQPower); 
+			}
+			///"Circular rotate right" permutation
+			/** "Circular rotate right" permutation - Subtracts argument from permutation state, carrying lowest permutations to highest. */
+			void RSUB(bitCapInt toSub) {
+				std::rotate(stateVec, stateVec + toSub, stateVec + maxQPower); 
+			}
+			///Add (without sign or carry)
+			void UADD(bitCapInt toAdd) {
+				RADD(toAdd);
+
+				bitCapInt lcv;
+				for (lcv = 0; lcv < toAdd; lcv++) {
+					runningNorm -= normSqrd(stateVec + lcv);
+					stateVec[lcv] = Complex16(0.0, 0.0);
+				}
+			}
+			///Subtract (without sign or carry)
+			void USUB(bitCapInt toSub) {
+				RSUB(toSub);
+
+				bitCapInt lcv;
+				for (lcv = 0; lcv < toSub; lcv++) {
+					runningNorm -= normSqrd(stateVec + maxQPower - lcv - 1);
+					stateVec[maxQPower - lcv - 1] = Complex16(0.0, 0.0);
+				}
+			}
 			///Arithmetic shift left, with index 0 bit as sign bit
 			void ASL(bitLenInt shift) {
 				if (shift > 0) {
