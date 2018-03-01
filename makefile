3d7dfe97427a70237e3cf061775ec503d019cb84
Daniel Strano <stranoj@gmail.com>
2018-02-28 21:12:10 -0500

    Fixing merge

--- makefile
+++ makefile
@@ -1,9 +1,10 @@
 # 1 to use OpenCL-based optimizations
-ENABLE_OPENCL ?= 0
+ENABLE_OPENCL ?= 1
 
 CPP      = g++
 XXD      = xxd
-OBJ      = complex16simd.o example.o qrack_base.o par_for.o
+OBJ      = complex16simd.o qrack.o example.o
+LINKOBJ  = complex16simd.o qrack.o example.o
 BIN      = example
 LIBS     = -lm -lpthread
 INCS     =
@@ -14,10 +15,10 @@ RM       = rm -f
 ifeq (${ENABLE_OPENCL},1)
   LIBS += -lOpenCL
   CXXFLAGS += -DENABLE_OPENCL=1
-  OBJ += qrack_ocl.o qrack.o
+  QRACKVER = qrack_ocl.cpp
 else
   CXXFLAGS += -DENABLE_OPENCL=0
-  OBJ += qrack.o
+  QRACKVER = qrack.cpp
 endif
 
 .PHONY: all all-before all-after clean clean-custom
@@ -28,11 +29,17 @@ clean: clean-custom
 	$(RM) $(OBJ) qrackcl.hpp
 
 $(BIN): $(OBJ)
-	$(CPP) $(OBJ) -o $(BIN) $(LIBS)
+	$(CPP) $(LINKOBJ) -o $(BIN) $(LIBS)
+
+complex16simd.o: complex16simd.cpp
+	$(CPP) -c complex16simd.cpp -o complex16simd.o $(CXXFLAGS)	
+
+qrack.o: $(QRACKVER)
+	$(CPP) -c $(QRACKVER) -o qrack.o $(CXXFLAGS)	
 
 ifeq (${ENABLE_OPENCL},1)
 qrackcl.hpp: qrack.cl
 	${XXD} -i qrack.cl > qrackcl.hpp
 
-qrack_ocl.o: qrackcl.hpp
+qrack.o: qrackcl.hpp
 endif
--- par_for.hpp
+++ par_for.hpp
@@ -175,9 +175,50 @@ void par_for_reg(const bitLenInt start, const bitLenInt length, const bitLenInt 
     }
 }
 
-double par_norm(const bitCapInt maxQPower, const Complex16* stateArray);
+double par_norm(const bitCapInt maxQPower, const Complex16* stateArray)
+{
+    // const double* sAD = reinterpret_cast<const double*>(stateArray);
+    // double* sSAD = new double[maxQPower * 2];
+    // std::partial_sort_copy(sAD, sAD + (maxQPower * 2), sSAD, sSAD + (maxQPower * 2));
+    // Complex16* sorted = reinterpret_cast<Complex16*>(sSAD);
 
-} // namespace Qrack
+    std::atomic<bitCapInt> idx;
+    idx = 0;
+    int num_cpus = std::thread::hardware_concurrency();
+    double* nrmPart = new double[num_cpus];
+    std::vector<std::future<void>> futures(num_cpus);
+    for (int cpu = 0; cpu != num_cpus; ++cpu) {
+        futures[cpu] = std::async(std::launch::async, [cpu, &idx, maxQPower, stateArray, nrmPart]() {
+            double sqrNorm = 0.0;
+            // double smallSqrNorm = 0.0;
+            bitCapInt i;
+            for (;;) {
+                i = idx++;
+                // if (i >= maxQPower) {
+                //	sqrNorm += smallSqrNorm;
+                //	break;
+                //}
+                // smallSqrNorm += norm(sorted[i]);
+                // if (smallSqrNorm > sqrNorm) {
+                //	sqrNorm += smallSqrNorm;
+                //	smallSqrNorm = 0;
+                //}
+                if (i >= maxQPower)
+                    break;
+                sqrNorm += norm(stateArray[i]);
+            }
+            nrmPart[cpu] = sqrNorm;
+        });
+    }
+
+    double nrmSqr = 0;
+    for (int cpu = 0; cpu != num_cpus; ++cpu) {
+        futures[cpu].get();
+        nrmSqr += nrmPart[cpu];
+    }
+    return sqrt(nrmSqr);
+}
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // THIS ENDS THE EXCERPTED SECTION FROM "GILGAMESH."
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
