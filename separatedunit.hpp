fbb97466518f2923329c7e4deca1abf2d70168b8
Daniel Strano <stranoj@gmail.com>
2018-04-05 12:38:17 -0400

    Bit list reduction comments

--- separatedunit.hpp
+++ separatedunit.hpp
@@ -59,7 +59,15 @@ protected:
     std::unique_ptr<QbLookup[]> qubitLookup;
     std::vector<CoherentUnit> coherentUnits;
 
+    /// Compile an order-preserving list of CoherentUnit bit strings for applying an register-wise operation
+    /**
+      * This operation optimizes compiling a list out of qubit pile when bit order is important. We apply register-wise operations over a pile of arbitrarily entangled and separated qubits. Entangled qubits are stored together in single CoherentUnit objects, but their mapping to SeparatedUnit bit indices can be generally random. Sometimes, we must preserve bit order to correctly carry out the operation, whereas sometimes our operation is bitwise parallel and does not depend on the ordering of bits in the list.
+      */
     void GetOrderedBitList(bitLenInt start, bitLenInt length, std::vector<QbListEntry> qbList);
+    /// Compile a list of CoherentUnit bit strings for applying a bitwise-parallel operation
+    /**
+      * This operation optimizes compiling a list out of qubit pile when bit order is not important. We apply register-wise operations over a pile of arbitrarily entangled and separated qubits. Entangled qubits are stored together in single CoherentUnit objects, but their mapping to SeparatedUnit bit indices can be generally random. Sometimes, we must preserve bit order to correctly carry out the operation, whereas sometimes our operation is bitwise parallel and does not depend on the ordering of bits in the list.
+      */
     void GetParallelBitList(bitLenInt start, bitLenInt length, std::vector<QbListEntry> qbList);
 };
 } // namespace Qrack
