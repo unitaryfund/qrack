bb541fd92a0e863b50b9d76f4be87a320559c550
bennbollay <bb.github@magitech.org>
2018-04-25 19:43:59 -0700

    Coverage and Format support in CMake (#51)
    * Support ‘make format’ to run clang-format-5.0 against the source files.
    * Support ‘coverage’ generation from the unittest results, instructions in README.md.

---
+++ cmake/Format.cmake
@@ -0,0 +1,20 @@
+set( FORMAT_EXCLUDE_FILES "catch.hpp" "_build")
+
+find_program ( CLANG_FORMAT clang-format-5.0 )
+file (GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.hpp)
+
+foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
+    foreach (EXCLUDE_FILE ${FORMAT_EXCLUDE_FILES})
+		string (FIND ${SOURCE_FILE} ${EXCLUDE_FILE} EXCLUDE)
+		if (NOT ${EXCLUDE} EQUAL -1)
+			list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
+		endif ()
+    endforeach ()
+endforeach ()
+
+add_custom_target (
+    format
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND ${CLANG_FORMAT} -style=file -i ${ALL_SOURCE_FILES}
+    )
+
