5031fabaffefcc967f9b2f242cd11e634ddda8e8
Daniel Strano <stranoj@gmail.com>
2018-05-05 15:19:52 -0400

    Micro-optimization #2

--- src/common/complex16x2simd.cpp
+++ src/common/complex16x2simd.cpp
@@ -67,10 +67,14 @@ Complex16x2Simd Complex16x2Simd::operator*=(const double& other)
 Complex16x2Simd dupeLo(const Complex16x2Simd& cmplx2) { return _mm256_permute2f128_pd(cmplx2._val2, cmplx2._val2, 0); }
 Complex16x2Simd dupeHi(const Complex16x2Simd& cmplx2) { return _mm256_permute2f128_pd(cmplx2._val2, cmplx2._val2, 17); }
 Complex16x2Simd matrixMul(const Complex16x2Simd& mtrxCol1, const Complex16x2Simd& mtrxCol2, const Complex16x2Simd& qubit) {
-    return _mm256_add_pd((mtrxCol1 * dupeLo(qubit))._val2, (mtrxCol2 * dupeHi(qubit))._val2);
+    __m256d dupeLo = _mm256_permute2f128_pd(qubit._val2, qubit._val2, 0);
+    __m256d dupeHi = _mm256_permute2f128_pd(qubit._val2, qubit._val2, 17);
+    return _mm256_add_pd(_mm256_add_pd(_mm256_mul_pd(_mm256_shuffle_pd(mtrxCol1._val2, mtrxCol1._val2, 5), _mm256_shuffle_pd(-dupeLo, dupeLo, 15)), _mm256_mul_pd(mtrxCol1._val2, _mm256_shuffle_pd(dupeLo, dupeLo, 0))), _mm256_add_pd(_mm256_mul_pd(_mm256_shuffle_pd(mtrxCol2._val2, mtrxCol2._val2, 5), _mm256_shuffle_pd(-dupeHi, dupeHi, 15)), _mm256_mul_pd(mtrxCol2._val2, _mm256_shuffle_pd(dupeHi, dupeHi, 0))));
 }
 Complex16x2Simd matrixMul(const double& nrm, const Complex16x2Simd& mtrxCol1, const Complex16x2Simd& mtrxCol2, const Complex16x2Simd& qubit) {
-    return _mm256_mul_pd(_mm256_set1_pd(nrm), _mm256_add_pd((mtrxCol1 * dupeLo(qubit))._val2, (mtrxCol2 * dupeHi(qubit))._val2));
+    __m256d dupeLo = _mm256_permute2f128_pd(qubit._val2, qubit._val2, 0);
+    __m256d dupeHi = _mm256_permute2f128_pd(qubit._val2, qubit._val2, 17);
+    return _mm256_mul_pd(_mm256_set1_pd(nrm),_mm256_add_pd(_mm256_add_pd(_mm256_mul_pd(_mm256_shuffle_pd(mtrxCol1._val2, mtrxCol1._val2, 5), _mm256_shuffle_pd(-dupeLo, dupeLo, 15)), _mm256_mul_pd(mtrxCol1._val2, _mm256_shuffle_pd(dupeLo, dupeLo, 0))), _mm256_add_pd(_mm256_mul_pd(_mm256_shuffle_pd(mtrxCol2._val2, mtrxCol2._val2, 5), _mm256_shuffle_pd(-dupeHi, dupeHi, 15)), _mm256_mul_pd(mtrxCol2._val2, _mm256_shuffle_pd(dupeHi, dupeHi, 0)))));
 }
 Complex16x2Simd operator*(const double lhs, const Complex16x2Simd& rhs) { return _mm256_mul_pd(_mm256_set1_pd(lhs), rhs._val2); }
 } // namespace Qrack
