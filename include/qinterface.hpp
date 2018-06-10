85c361341a53eda6a432c65d997c0f3c506fd9eb
WrathfulSpatula <stranoj@gmail.com>
2018-06-09 20:36:39 -0400

    Reverting flag name

--- include/qinterface.hpp
+++ include/qinterface.hpp
@@ -16,21 +16,17 @@
 #include <math.h>
 #include <memory>
 #include <vector>
-#include <complex>
-
 #define bitLenInt uint8_t
 #define bitCapInt uint64_t
 #define bitsInByte 8
 
-#ifndef ENABLE_COMPLEX8
-#include "common/complex16simd.hpp"
-#endif
-
-#ifdef ENABLE_COMPLEX8
+#if ENABLE_COMPLEX8
+    #include <complex>
     #define complex std::complex<float>
     #define real1 float
     #define min_norm 1e-9
 #else
+    #include "common/complex16simd.hpp"
     #define complex Complex16Simd
     #define real1 double
     #define min_norm 1e-15
