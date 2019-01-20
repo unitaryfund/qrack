a3bc542d178d99287a2f308bd8e3e0edbf16a4b9
WrathfulSpatula <stranoj@gmail.com>
2019-01-20 13:36:33 -0500

    Fixing double build

--- src/qengine/state.cpp
+++ src/qengine/state.cpp
@@ -310,8 +310,8 @@ void QEngineCPU::UniformlyControlledSingleBit(
         qubit[0] = nrm * ((mtrxs[0 + offset] * Y0) + (mtrxs[1 + offset] * qubit[1]));
         qubit[1] = nrm * ((mtrxs[2 + offset] * Y0) + (mtrxs[3 + offset] * qubit[1]));
 
-        real1 nrm1 = std::norm(qubit[0]);
-        real1 nrm2 = std::norm(qubit[1]);
+        real1 nrm1 = norm(qubit[0]);
+        real1 nrm2 = norm(qubit[1]);
         if (nrm1 < min_norm) {
             nrm1 = ZERO_R1;
             qubit[0] = complex(ZERO_R1, ZERO_R1);
