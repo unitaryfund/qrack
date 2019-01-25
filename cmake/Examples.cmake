4142b916f599aee595066a9ceaa6066dd9172de6
WrathfulSpatula <stranoj@gmail.com>
2019-01-25 00:31:19 -0500

    Adding ordered list search example; changing Doxygen version number

--- cmake/Examples.cmake
+++ cmake/Examples.cmake
@@ -20,5 +20,18 @@ target_link_libraries (grovers_lookup
     pthread
     )
 
+add_executable (ordered_list_search
+    examples/ordered_list_search.cpp
+    )
+
+set_target_properties(ordered_list_search PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/examples")
+
+target_link_libraries (ordered_list_search
+    qrack
+    pthread
+    )
+
+
 target_compile_options (grovers PUBLIC -O3 -std=c++11 -Wall -Werror ${TEST_COMPILE_OPTS} -DCATCH_CONFIG_FAST_COMPILE)
 target_compile_options (grovers_lookup PUBLIC -O3 -std=c++11 -Wall -Werror ${TEST_COMPILE_OPTS} -DCATCH_CONFIG_FAST_COMPILE)
+target_compile_options (ordered_list_search PUBLIC -O3 -std=c++11 -Wall -Werror ${TEST_COMPILE_OPTS} -DCATCH_CONFIG_FAST_COMPILE)
