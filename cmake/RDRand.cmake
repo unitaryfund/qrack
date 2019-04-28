4a79e6afb3b32ccbe0baa98dbb7a4434e78c3af7
WrathfulSpatula <stranoj@gmail.com>
2019-04-27 21:18:59 -0400

    ENABLE_RDRAND CMake message

--- cmake/RDRand.cmake
+++ cmake/RDRand.cmake
@@ -4,3 +4,5 @@ if (ENABLE_RDRAND)
     set(QRACK_COMPILE_OPTS ${QRACK_COMPILE_OPTS} -mrdrnd)
     target_compile_definitions(qrack PUBLIC ENABLE_RDRAND=1)
 endif (ENABLE_RDRAND)
+
+message ("Try RDRAND is: ${ENABLE_RDRAND}")
