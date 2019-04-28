f429f8370809e76869dce9aa1efda61a9b22b0eb
WrathfulSpatula <stranoj@gmail.com>
2019-04-27 21:31:06 -0400

    Hide RDRAND includes if disabled

--- include/common/rdrandwrapper.hpp
+++ include/common/rdrandwrapper.hpp
@@ -10,8 +10,10 @@
 // See LICENSE.md in the project root or https://www.gnu.org/licenses/lgpl-3.0.en.html
 // for details.
 
+#if ENABLE_RDRAND
 #include <cpuid.h>
 #include <immintrin.h>
+#endif
 
 #pragma once
 
