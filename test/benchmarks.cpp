a086538053fbc51728e38569c9132394483f56ab
WrathfulSpatula <stranoj@gmail.com>
2019-05-24 20:36:00 -0400

    Reverting benchmarks.cpp

--- test/benchmarks.cpp
+++ test/benchmarks.cpp
@@ -142,7 +142,7 @@ void benchmarkLoop(
 {
     benchmarkLoopVariable(fn, MaxQubits, resetRandomPerm, hadamardRandomBits);
 }
-#if 0
+
 TEST_CASE("test_cnot_single")
 {
     benchmarkLoop([](QInterfacePtr qftReg, int n) { qftReg->CNOT(0, 1, 1); });
@@ -360,7 +360,7 @@ TEST_CASE("test_qft_ideal_init")
 {
     benchmarkLoop([](QInterfacePtr qftReg, int n) { qftReg->QFT(0, n, false); }, false, false);
 }
-#endif
+
 TEST_CASE("test_qft_permutation_init")
 {
     benchmarkLoop([](QInterfacePtr qftReg, int n) { qftReg->QFT(0, n, false); }, true, false);
