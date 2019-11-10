8e10ed167a1897c76e605c934b040f76bddc5f24
WrathfulSpatula <stranoj@gmail.com>
2019-11-10 00:39:14 -0500

    No local region privileged

--- test/benchmarks.cpp
+++ test/benchmarks.cpp
@@ -637,11 +637,11 @@ TEST_CASE("test_cosmology_2", "[cosmos]")
         [](QInterfacePtr qUniverse, int n) {
             int t, x;
             for (t = 1; t < n; t++) {
-                for (x = 0; x < (n / t); x++) {
+                for (x = 0; x < n; x++) {
                     if (qUniverse->Rand() < (ONE_R1 / 2)) {
-                        qUniverse->QFT(x * t, t);
+                        qUniverse->QFT(x, t);
                     } else {
-                        qUniverse->IQFT(x * t, t);
+                        qUniverse->IQFT(x, t);
                     }
                 }
             }
