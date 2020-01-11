f5ac7371cc598bc2a2af75f7c2991af606d4aa3f
WrathfulSpatula <stranoj@gmail.com>
2020-01-10 23:47:51 -0500

    No identical consecutive single bit gates

--- test/benchmarks.cpp
+++ test/benchmarks.cpp
@@ -673,6 +673,10 @@ TEST_CASE("test_quantum_supremacy", "[supreme]")
 
         bitLenInt controls[1];
 
+        std::vector<int> lastSingleBitGates;
+        std::set<int>::iterator gateChoiceIterator;
+        int gateChoice;
+
         // We repeat the entire prepartion for "depth" iterations.
         // We can avoid maximal representational entanglement of the state as a single Schr{\"o}dinger method unit.
         // See https://arxiv.org/abs/1710.05867
@@ -683,14 +687,51 @@ TEST_CASE("test_quantum_supremacy", "[supreme]")
                 // Each individual bit has one of these 3 gates applied at random.
                 // Qrack has optimizations for gates including X, Y, and particularly H, but these "Sqrt" variants
                 // are handled as general single bit gates.
-                if (gateRand < (ONE_R1 / 3)) {
-                    qReg->SqrtX(i);
-                } else if (gateRand < (2 * ONE_R1 / 3)) {
-                    qReg->SqrtY(i);
+
+                // The same gate is not applied twice consecutively in sequence.
+
+                if (d == 0) {
+                    // For the first iteration, we can pick any gate.
+
+                    if (gateRand < (ONE_R1 / 3)) {
+                        qReg->SqrtX(i);
+                        lastSingleBitGates.push_back(0);
+                    } else if (gateRand < (2 * ONE_R1 / 3)) {
+                        qReg->SqrtY(i);
+                        lastSingleBitGates.push_back(1);
+                    } else {
+                        // "Square root of W" appears to be equivalent to T.SqrtX.IT, looking at the definition in the
+                        // supplemental materials.
+                        qReg->SqrtXConjT(i);
+                        lastSingleBitGates.push_back(2);
+                    }
                 } else {
-                    // "Square root of W" appears to be equivalent to T.SqrtX.IT, looking at the definition in the
-                    // supplemental materials.
-                    qReg->SqrtXConjT(i);
+                    // For all subsequent iterations after the first, we eliminate the choice of the same gate applied
+                    // on the immediately previous iteration.
+
+                    std::set<int> gateChoices = { 0, 1, 2 };
+                    gateChoiceIterator = gateChoices.begin();
+                    std::advance(gateChoiceIterator, lastSingleBitGates[i]);
+                    gateChoices.erase(gateChoiceIterator);
+
+                    gateChoiceIterator = gateChoices.begin();
+                    std::advance(gateChoiceIterator, (gateRand < (ONE_R1 / 2)) ? 0 : 1);
+                    gateChoices.erase(gateChoiceIterator);
+
+                    gateChoice = *(gateChoices.begin());
+
+                    if (gateChoice == 0) {
+                        qReg->SqrtX(i);
+                        lastSingleBitGates[i] = 0;
+                    } else if (gateChoice == 1) {
+                        qReg->SqrtY(i);
+                        lastSingleBitGates[i] = 1;
+                    } else {
+                        // "Square root of W" appears to be equivalent to T.SqrtX.IT, looking at the definition in the
+                        // supplemental materials.
+                        qReg->SqrtXConjT(i);
+                        lastSingleBitGates[i] = 2;
+                    }
                 }
 
                 // This is a QUnit specific optimization attempt method that can "compress" (or "Schmidt decompose")
