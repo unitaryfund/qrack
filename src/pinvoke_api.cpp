fcb4feac8b460897d80657b0c4ff6f2bc02281ab
Daniel Strano <stranoj@gmail.com>
2020-02-29 18:09:47 -0500

    Fixing R implementation

--- src/pinvoke_api.cpp
+++ src/pinvoke_api.cpp
@@ -443,16 +443,16 @@ MICROSOFT_QUANTUM_DECL void R(_In_ unsigned sid, _In_ unsigned b, _In_ double ph
 
 	switch (b) {
 	case PauliI:
-		simulator->RT(phi, shards[simulator][q]);
+		simulator->Exp(phi, shards[simulator][q]);
 		break;
 	case PauliX:
-		simulator->RX(phi, shards[simulator][q]);
+		simulator->ExpX(phi, shards[simulator][q]);
 		break;
 	case PauliY:
-		simulator->RY(phi, shards[simulator][q]);
+		simulator->ExpY(phi, shards[simulator][q]);
 		break;
 	case PauliZ:
-		simulator->RZ(phi, shards[simulator][q]);
+		simulator->ExpZ(phi, shards[simulator][q]);
 		break;
 	default:
 		break;
@@ -470,34 +470,30 @@ MICROSOFT_QUANTUM_DECL void MCR(_In_ unsigned sid, _In_ unsigned b, _In_ double 
 		ctrlsArray[i] = shards[simulator][c[i]];
 	}
 
-	real1 cosine = cos(phi / 2.0);
-	real1 sine = sin(phi / 2.0);
-	complex pauliR[4];
+	const complex pauliI[4] = { ONE_CMPLX, ZERO_CMPLX, ZERO_CMPLX, ONE_CMPLX };
+	const complex pauliX[4] = { ZERO_CMPLX, ONE_CMPLX, ONE_CMPLX, ZERO_CMPLX };
+	const complex pauliY[4] = { ZERO_CMPLX, -I_CMPLX, I_CMPLX, ZERO_CMPLX };
+	const complex pauliZ[4] = { ONE_CMPLX, ZERO_CMPLX, ZERO_CMPLX, -ONE_CMPLX };
+
+	complex toApply[4];
 
 	switch (b) {
 	case PauliI:
-		simulator->ApplyControlledSinglePhase(ctrlsArray, n, shards[simulator][q], complex(ONE_R1, ZERO_R1), complex(cosine, sine));
+		mul2x2(phi / 2, pauliI, toApply);
 		break;
 	case PauliX:
-		pauliR[0] = complex(cosine, ZERO_R1);
-		pauliR[1] = complex(ZERO_R1, -sine);
-		pauliR[2] = complex(ZERO_R1, -sine);
-		pauliR[3] = complex(cosine, ZERO_R1);
-		simulator->ApplyControlledSingleBit(ctrlsArray, n, shards[simulator][q], pauliR);
+		mul2x2(phi / 2, pauliX, toApply);
 		break;
 	case PauliY:
-		pauliR[0] = complex(cosine, ZERO_R1);
-		pauliR[1] = complex(-sine, ZERO_R1);
-		pauliR[2] = complex(sine, ZERO_R1);
-		pauliR[3] = complex(cosine, ZERO_R1);
-		simulator->ApplyControlledSingleBit(ctrlsArray, n, shards[simulator][q], pauliR);
+		mul2x2(phi / 2, pauliY, toApply);
 		break;
 	case PauliZ:
-		simulator->ApplyControlledSinglePhase(ctrlsArray, n, shards[simulator][q], complex(cosine, -sine), complex(cosine, sine));
+		mul2x2(phi / 2, pauliZ, toApply);
 		break;
 	default:
 		break;
 	}
+	simulator->Exp(ctrlsArray, n, shards[simulator][q], toApply);
 
 	delete[] ctrlsArray;
 }
