3e77ac1b54bd857d45be30f9679b63bc662fb28d
WrathfulSpatula <stranoj@gmail.com>
2020-05-10 11:35:29 -0400

    Revert 1b90c60

--- src/qunit.cpp
+++ src/qunit.cpp
@@ -1455,10 +1455,8 @@ void QUnit::ApplySingleInvert(const complex topRight, const complex bottomLeft, 
         return;
     }
 
-    RevertBasis2Qb(target, ONLY_INVERT, ONLY_TARGETS, CTRL_AND_ANTI);
-    // These arguments are intentionally flipped, because we're commuting phase at this point in the logic for an
-    // inversion gate.
-    shard.CommutePhase(bottomLeft, topRight);
+    // TODO: Incorporate phase effects, to commute isInvert controls.
+    RevertBasis2Qb(target, ONLY_INVERT, CONTROLS_AND_TARGETS, CTRL_AND_ANTI);
     shard.FlipPhaseAnti();
 
     if (!shard.isPlusMinus) {
