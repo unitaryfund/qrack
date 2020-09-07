6594c4739ad37c2ee5b2f61e9a16baab4d73302c
Daniel Strano <stranoj@gmail.com>
2020-09-07 14:57:07 -0400

    Restore QUnitMulti/QPager/QEngineOCL benchmarks

--- test/benchmarks_main.cpp
+++ test/benchmarks_main.cpp
@@ -275,6 +275,14 @@ int main(int argc, char* argv[])
             CreateQuantumInterface(QINTERFACE_OPENCL, 1, 0).reset(); /* Get the OpenCL banner out of the way. */
             num_failed = session.run();
         }
+
+        if (num_failed == 0 && opencl_multi) {
+            session.config().stream() << "############ QUnitMulti -> QPager (OpenCL) ############" << std::endl;
+            testEngineType = QINTERFACE_QUNIT_MULTI;
+            testSubSubEngineType = QINTERFACE_OPENCL;
+            CreateQuantumInterface(QINTERFACE_OPENCL, 1, 0).reset(); /* Get the OpenCL banner out of the way. */
+            num_failed = session.run();
+        }
 #endif
     }
 
