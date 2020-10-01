1f85b374172f2628b7ac7d090000e81ac2fa26c5
Daniel Strano <stranoj@gmail.com>
2020-10-01 08:14:47 -0400

    Only handle edge case for stabilizer

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -381,8 +381,8 @@ void QStabilizerHybrid::ApplySingleBit(const complex* mtrx, bitLenInt target)
         return;
     }
 
-    if (IS_SAME(mtrx[0], complex(ONE_R1 / 2, ONE_R1 / 2)) && IS_SAME(mtrx[1], complex(ONE_R1 / 2, -ONE_R1 / 2)) &&
-        IS_SAME(mtrx[0], mtrx[3]) && IS_SAME(mtrx[1], mtrx[2])) {
+    if (stabilizer && IS_SAME(mtrx[0], complex(ONE_R1 / 2, ONE_R1 / 2)) &&
+        IS_SAME(mtrx[1], complex(ONE_R1 / 2, -ONE_R1 / 2)) && IS_SAME(mtrx[0], mtrx[3]) && IS_SAME(mtrx[1], mtrx[2])) {
         IS(target);
         H(target);
         IS(target);
