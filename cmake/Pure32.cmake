693070702bf708eb9de0fbc9408f0c8d66d9ab9e
WrathfulSpatula <stranoj@gmail.com>
2021-02-07 15:00:15 -0500

    Make int masking per node independent from distributed max qubits

--- cmake/Pure32.cmake
+++ cmake/Pure32.cmake
@@ -1,5 +1,6 @@
 option (ENABLE_PURE32 "Use only 32-bit types or smaller" OFF)
 option (ENABLE_VC4CL "Build a library version that's safe for the VC4CL compiler (for the Raspberry Pi 3)" OFF)
+option (ENABLE_INT32 "Use 32-bit (instead of 64-bit) integer types for coherent addressable qubit masks" OFF)
 
 if (ENABLE_VC4CL)
     set(ENABLE_PURE32 ON)
@@ -9,6 +10,10 @@ endif(ENABLE_VC4CL)
 if (ENABLE_PURE32 OR (QBCAPPOW EQUAL 5))
     set(ENABLE_COMPLEX_X2 OFF)
     set(ENABLE_COMPLEX8 ON)
+    set(ENABLE_INT32 ON)
     set(QBCAPPOW "5")
-    target_compile_definitions (qrack PUBLIC ENABLE_PURE32=1)
 endif (ENABLE_PURE32 OR (QBCAPPOW EQUAL 5))
+
+if (ENABLE_INT32)
+    target_compile_definitions (qrack PUBLIC ENABLE_PURE32=1)
+endif (ENABLE_INT32)
