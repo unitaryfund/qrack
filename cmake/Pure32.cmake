9cbcf589ba1d5bbad522ee3768e288d921c8fbb4
WrathfulSpatula <stranoj@gmail.com>
2021-02-07 15:22:11 -0500

    Rename ENABLE_UINT32

--- cmake/Pure32.cmake
+++ cmake/Pure32.cmake
@@ -1,6 +1,6 @@
 option (ENABLE_PURE32 "Use only 32-bit types or smaller" OFF)
 option (ENABLE_VC4CL "Build a library version that's safe for the VC4CL compiler (for the Raspberry Pi 3)" OFF)
-option (ENABLE_INT32 "Use 32-bit (instead of 64-bit) integer types for coherent addressable qubit masks" OFF)
+option (ENABLE_UINT32 "Use 32-bit (instead of 64-bit) unsigned integer types for coherent addressable qubit masks" OFF)
 
 if (ENABLE_VC4CL)
     set(ENABLE_PURE32 ON)
@@ -14,6 +14,6 @@ if (ENABLE_PURE32 OR (QBCAPPOW EQUAL 5))
     set(QBCAPPOW "5")
 endif (ENABLE_PURE32 OR (QBCAPPOW EQUAL 5))
 
-if (ENABLE_INT32)
+if (ENABLE_UINT32)
     target_compile_definitions (qrack PUBLIC ENABLE_PURE32=1)
-endif (ENABLE_INT32)
+endif (ENABLE_UINT32)
