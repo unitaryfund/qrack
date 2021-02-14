4151c6f14b8095fa13c687a3d3836c6f92ebdd61
WrathfulSpatula <stranoj@gmail.com>
2021-02-14 12:51:21 -0500

    Fix half REAL1_EPSILON

--- include/common/qrack_types.hpp
+++ include/common/qrack_types.hpp
@@ -78,7 +78,7 @@ typedef float real1_f;
 #define ONE_R1 ((real1)1.0f)
 #define PI_R1 ((real1)M_PI)
 #define REAL1_DEFAULT_ARG ((real1)-999.0f)
-#define REAL1_EPSILON ((real1)1.0e-10f)
+#define REAL1_EPSILON ((real1)5.9604645e-08f)
 } // namespace Qrack
 #elif FPPOW < 6
 namespace Qrack {
