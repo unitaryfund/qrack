6fe76f1dabcea460eec62c3f0da708739b0631ab
WrathfulSpatula <stranoj@gmail.com>
2021-02-16 07:55:52 -0500

    Fix QPager w/ QEngineOCL Clone()

--- src/qengine/opencl.cpp
+++ src/qengine/opencl.cpp
@@ -117,12 +117,12 @@ void QEngineOCL::SetAmplitudePage(
     QEngineOCLPtr pageEngineOclPtr = std::dynamic_pointer_cast<QEngineOCL>(pageEnginePtr);
     BufferPtr oStateBuffer = pageEngineOclPtr->stateBuffer;
 
+    clFinish();
+    pageEngineOclPtr->clFinish();
+
     if (!stateBuffer && !oStateBuffer) {
         return;
     }
-
-    clFinish();
-    pageEngineOclPtr->clFinish();
 
     if (!oStateBuffer) {
         if (length == maxQPower) {
@@ -130,6 +130,9 @@ void QEngineOCL::SetAmplitudePage(
         } else {
             ClearBuffer(stateBuffer, (bitCapIntOcl)dstOffset, (bitCapIntOcl)length);
         }
+
+        runningNorm = ZERO_R1;
+
         return;
     }
 
