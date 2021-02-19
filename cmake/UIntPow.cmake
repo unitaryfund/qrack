e0c45ba772bcc8d28acba2b18abd8f1f876771bc
WrathfulSpatula <stranoj@gmail.com>
2021-02-19 17:53:02 -0500

    Fix -DUINTPOW=6 default

--- cmake/UIntPow.cmake
+++ cmake/UIntPow.cmake
@@ -1,4 +1,4 @@
-set(UINTPOW "5" CACHE STRING "Log2 of \"local qubit\" capacity in QEngine types (not QPager or QUnit, must be at least 4, equivalent to short masks)")
+set(UINTPOW "6" CACHE STRING "Log2 of \"local qubit\" capacity in QEngine types (not QPager or QUnit, must be at least 4, equivalent to short masks)")
 if (UINTPOW LESS 4)
     message(FATAL_ERROR "UINTPOW must be at least 4, equivalent to \"short\" masks!")
 endif (UINTPOW LESS 4)
