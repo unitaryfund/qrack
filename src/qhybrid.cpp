8af94701bc4dc7589aa7c48c7b509a4f41389fa4
WrathfulSpatula <stranoj@gmail.com>
2021-02-22 22:18:27 -0500

    OpenCL macro switch-off

--- src/qhybrid.cpp
+++ src/qhybrid.cpp
@@ -35,10 +35,14 @@ QHybrid::QHybrid(bitLenInt qBitCount, bitCapInt initState, qrack_rand_gen_ptr rg
     } else {
         // Single bit gates act pairwise on amplitudes, so add at least 1 qubit to the log2 of the preferred
         // concurrency.
+#if ENABLE_OPENCL
         bitLenInt gpuQubits = log2(OCLEngine::Instance()->GetDeviceContextPtr(devID)->GetPreferredConcurrency()) + 1U;
         bitLenInt cpuQubits = (concurrency == 1 ? PSTRIDEPOW : (log2(concurrency - 1) + PSTRIDEPOW + 1));
 
         thresholdQubits = gpuQubits < cpuQubits ? gpuQubits : cpuQubits;
+#else
+        thresholdQubits = (concurrency == 1 ? PSTRIDEPOW : (log2(concurrency - 1) + PSTRIDEPOW + 1));
+#endif
     }
 
     isGpu = (qubitCount >= thresholdQubits);
