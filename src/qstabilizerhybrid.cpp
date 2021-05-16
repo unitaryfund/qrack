0d0636f7c9b4c0068acd16b6ff0e7da558fed7be
Daniel Strano <stranoj@gmail.com>
2021-05-16 14:42:10 -0400

    Testing CI

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -338,7 +338,7 @@ void QStabilizerHybrid::Dispose(bitLenInt start, bitLenInt length)
         return;
     }
 
-    if (stabilizer && !stabilizer->CanDecomposeDispose(start, length)) {
+    if ((length > 1U) || (stabilizer && !stabilizer->CanDecomposeDispose(start, 1))) {
         SwitchToEngine();
     }
 
@@ -366,7 +366,7 @@ void QStabilizerHybrid::Dispose(bitLenInt start, bitLenInt length, bitCapInt dis
         return;
     }
 
-    if (stabilizer && !stabilizer->CanDecomposeDispose(start, length)) {
+    if ((length > 1U) || (stabilizer && !stabilizer->CanDecomposeDispose(start, 1))) {
         SwitchToEngine();
     }
 
