98cd28c9c662b8b3168c109a66505c3e9820211f
Daniel Strano <stranoj@gmail.com>
2021-06-20 12:43:48 -0400

    Fix div by 0

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -415,7 +415,8 @@ void QStabilizerHybrid::SetQuantumState(const complex* inputState)
         real1 sqrtProb = abs(inputState[1]);
         real1 sqrt1MinProb = abs(inputState[0]);
         complex probMatrix[4] = { sqrt1MinProb, sqrtProb, sqrtProb, -sqrt1MinProb };
-        complex phaseMatrix[4] = { inputState[0] / sqrt1MinProb, ZERO_CMPLX, ZERO_CMPLX, inputState[1] / sqrtProb };
+        complex phaseMatrix[4] = { (sqrt1MinProb <= FP_NORM_EPSILON) ? ONE_CMPLX : (inputState[0] / sqrt1MinProb),
+            ZERO_CMPLX, ZERO_CMPLX, (sqrtProb <= FP_NORM_EPSILON) ? ONE_CMPLX : (inputState[1] / sqrtProb) };
         shards[0] = std::make_shared<QStabilizerShard>(probMatrix);
         shards[0]->Compose(phaseMatrix);
 
