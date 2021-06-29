25c0e33acd60c64abfae58ade0611c18534dff3f
Daniel Strano <stranoj@gmail.com>
2021-06-29 09:34:44 -0400

    Corrected complementary self-adjoint case

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -417,6 +417,12 @@ void QStabilizerHybrid::ApplySingleBit(const complex* lMtrx, bitLenInt target)
         return;
     }
 
+    if (IS_SAME(mtrx[0], -I_CMPLX * mtrx[1]) && IS_SAME(mtrx[0], I_CMPLX * mtrx[2]) && IS_SAME(mtrx[0], -mtrx[3])) {
+        stabilizer->Z(target);
+        stabilizer->SqrtX(target);
+        return;
+    }
+
     shards[target] = std::make_shared<QStabilizerShard>(mtrx);
     CacheEigenstate(target);
 }
