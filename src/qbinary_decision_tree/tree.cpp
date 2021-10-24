7783fdc3beb431b3eec8e6f107d1de16605a1b8f
Daniel Strano <stranoj@gmail.com>
2021-10-24 15:44:57 -0400

    Refactor

--- src/qbinary_decision_tree/tree.cpp
+++ src/qbinary_decision_tree/tree.cpp
@@ -469,15 +469,12 @@ bool QBinaryDecisionTree::ForceM(bitLenInt qubit, bool result, bool doForce, boo
 
 void QBinaryDecisionTree::Apply2x2OnLeaf(const complex* mtrx, QBinaryDecisionTreeNodePtr leaf)
 {
-    bool isBranch0Norm0 = IS_NORM_0(leaf->branches[0]->scale);
-    bool isBranch1Norm0 = IS_NORM_0(leaf->branches[1]->scale);
-
-    if (isBranch0Norm0) {
+    if (IS_NORM_0(leaf->branches[0]->scale)) {
         leaf->branches[0] = leaf->branches[1]->ShallowClone();
         leaf->branches[0]->scale = ZERO_CMPLX;
     }
 
-    if (isBranch1Norm0) {
+    if (IS_NORM_0(leaf->branches[1]->scale)) {
         leaf->branches[1] = leaf->branches[0]->ShallowClone();
         leaf->branches[1]->scale = ZERO_CMPLX;
     }
