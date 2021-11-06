e0f5d23ef5d6e2e8f77193672bf1f93f7bee6c18
Daniel Strano <stranoj@gmail.com>
2021-11-06 10:14:41 -0400

    Can't skip not iSameAtTop

--- src/qbinary_decision_tree/node.cpp
+++ src/qbinary_decision_tree/node.cpp
@@ -129,11 +129,6 @@ void QBinaryDecisionTreeNode::Prune(bitLenInt depth)
         if (IS_NORM_0(sb0->scale - sb1->scale) && (sb0->branches[bit] == sb1->branches[bit])) {
             sb1 = sb0;
         }
-
-        // WARNING: Mutates loop control variable!!!
-        if (!isSameAtTop) {
-            i |= powJ;
-        }
     }
 
     if (isSameAtTop) {
