fff93f24ba6cfad90d211d26a1f29b02656e1e04
Daniel Strano <stranoj@gmail.com>
2021-11-27 12:35:58 -0500

    Fix non-OCL builds

--- include/qpager.hpp
+++ include/qpager.hpp
@@ -341,10 +341,12 @@ public:
             qPages[i]->SetDevice(dID, forceReInit);
         }
 
+#if ENABLE_OPENCL
         if (rootEngine != QINTERFACE_CPU) {
             maxPageQubits = log2(OCLEngine::Instance()->GetDeviceContextPtr(devID)->GetMaxAlloc() / sizeof(complex)) -
                 segmentGlobalQb;
         }
+#endif
 
         if (!useGpuThreshold) {
             return;
