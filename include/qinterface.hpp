141cf719acca1207ba0baad2cb64813469c17c51
Daniel Strano <stranoj@gmail.com>
2022-02-17 18:04:55 -0500

    Move Compose(q, start) defintion out of header

--- include/qinterface.hpp
+++ include/qinterface.hpp
@@ -359,17 +359,7 @@ public:
      */
     virtual bitLenInt Compose(QInterfacePtr toCopy) { return Compose(toCopy, qubitCount); }
     virtual std::map<QInterfacePtr, bitLenInt> Compose(std::vector<QInterfacePtr> toCopy);
-    virtual bitLenInt Compose(QInterfacePtr toCopy, bitLenInt start)
-    {
-        bitLenInt origSize = qubitCount;
-        ROL(origSize - start, 0, origSize);
-
-        bitLenInt result = Compose(toCopy);
-
-        ROR(origSize - start, 0, qubitCount);
-
-        return result;
-    }
+    virtual bitLenInt Compose(QInterfacePtr toCopy, bitLenInt start);
 
     /**
      * Minimally decompose a set of contiguous bits from the separably composed unit,
