35d637432926ef4c337151716318d88d6fe05228
Daniel Strano <stranoj@gmail.com>
2022-03-26 16:33:49 -0400

    Update erroneous benchmark stat comment

--- test/benchmarks.cpp
+++ test/benchmarks.cpp
@@ -1232,7 +1232,7 @@ TEST_CASE("test_stabilizer_t_nn_d", "[supreme]")
     if (benchmarkMaxMagic >= 0) {
         std::cout << "(max quantum \"magic\": " << benchmarkMaxMagic << ")";
     } else {
-        std::cout << "(max quantum \"magic\": default, no ceiling)";
+        std::cout << "(max quantum \"magic\": default, ceiling equal to qubit count)";
     }
 
     const int DimCount1Qb = 4;
