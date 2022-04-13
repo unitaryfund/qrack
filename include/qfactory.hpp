e9f505f3e39bd1de17a86d60210c21dca7b5b59c
Daniel Strano <stranoj@gmail.com>
2022-04-13 10:06:28 -0400

    Wrap default layer arrangement in factory

--- include/qfactory.hpp
+++ include/qfactory.hpp
@@ -187,4 +187,61 @@ template <typename... Ts> QInterfacePtr CreateQuantumInterface(std::vector<QInte
     }
 }
 
+template <typename... Ts>
+QInterfacePtr CreateArrangedLayers(bool md, bool sd, bool sh, bool bdt, bool pg, bool zxf, bool hy, bool oc, Ts... args)
+{
+#if ENABLE_OPENCL
+    bool isOcl = oc && (OCLEngine::Instance().GetDeviceCount() > 0);
+    bool isOclMulti = oc && md && (OCLEngine::Instance().GetDeviceCount() > 1);
+#else
+    bool isOclMulti = false;
+#endif
+
+    // Construct backwards, then reverse:
+    std::vector<QInterfaceEngine> simulatorType;
+
+#if ENABLE_OPENCL
+    if (!hy || !isOcl) {
+        simulatorType.push_back(isOcl ? QINTERFACE_OPENCL : QINTERFACE_CPU);
+    }
+#endif
+
+    if (pg && !sh && simulatorType.size()) {
+        simulatorType.push_back(QINTERFACE_QPAGER);
+    }
+
+    if (zxf) {
+        simulatorType.push_back(QINTERFACE_MASK_FUSION);
+    }
+
+    if (bdt) {
+        simulatorType.push_back(QINTERFACE_BDT);
+    }
+
+    if (sh && (!sd || simulatorType.size())) {
+        simulatorType.push_back(QINTERFACE_STABILIZER_HYBRID);
+    }
+
+    if (sd) {
+        simulatorType.push_back(isOclMulti ? QINTERFACE_QUNIT_MULTI : QINTERFACE_QUNIT);
+    }
+
+    // (...then reverse:)
+    std::reverse(simulatorType.begin(), simulatorType.end());
+
+    if (!simulatorType.size()) {
+#if ENABLE_OPENCL
+        if (hy && isOcl) {
+            simulatorType.push_back(QINTERFACE_HYBRID);
+        } else {
+            simulatorType.push_back(isOcl ? QINTERFACE_OPENCL : QINTERFACE_CPU);
+        }
+#else
+        simulatorType.push_back(QINTERFACE_CPU);
+#endif
+    }
+
+    return CreateQuantumInterface(simulatorType, args...);
+}
+
 } // namespace Qrack
