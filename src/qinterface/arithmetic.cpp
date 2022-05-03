fe29b1ef85c9b41532ad3bf17b5f3290cc06e051
Daniel Strano <stranoj@gmail.com>
2022-05-03 12:49:38 -0400

    Negate power-of-2 check condition

--- src/qinterface/arithmetic.cpp
+++ src/qinterface/arithmetic.cpp
@@ -229,7 +229,7 @@ void QInterface::CMULModNOut(bitCapInt toMul, bitCapInt modN, bitLenInt inStart,
     const bitLenInt* controls, bitLenInt controlLen)
 {
     // See https://stackoverflow.com/questions/108318/how-can-i-test-whether-a-number-is-a-power-of-2#answer-108360
-    if (modN && !(modN & (modN - 1))) {
+    if (modN && (modN & (modN - 1))) {
         throw std::invalid_argument("CMULModNOut decomposition only implemented for mod N powers of 2!");
     }
     const bitLenInt oLength = log2(modN);
@@ -248,7 +248,8 @@ void QInterface::CMULModNOut(bitCapInt toMul, bitCapInt modN, bitLenInt inStart,
 void QInterface::CIMULModNOut(bitCapInt toMul, bitCapInt modN, bitLenInt inStart, bitLenInt outStart, bitLenInt length,
     const bitLenInt* controls, bitLenInt controlLen)
 {
-    if (modN && !(modN & (modN - 1))) {
+    // See https://stackoverflow.com/questions/108318/how-can-i-test-whether-a-number-is-a-power-of-2#answer-108360
+    if (modN && (modN & (modN - 1U))) {
         throw std::invalid_argument("CIMULModNOut decomposition only implemented for mod N powers of 2!");
     }
     const bitLenInt oLength = log2(modN);
