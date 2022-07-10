19f1754aa8d896c89ba66e7692e2fea16c9f2077
Daniel Strano <stranoj@gmail.com>
2022-07-10 12:35:11 -0400

    Edge case in pageMaxQPower()

--- include/qpager.hpp
+++ include/qpager.hpp
@@ -54,7 +54,7 @@ protected:
         basePageMaxQPower = pow2Ocl(baseQubitsPerPage);
     }
 
-    bitCapInt pageMaxQPower() { return maxQPower / qPages.size(); }
+    bitCapInt pageMaxQPower() { return qPages.size() ? (maxQPower / qPages.size()) : ONE_BCI; }
     bitLenInt pagedQubitCount() { return log2((bitCapInt)qPages.size()); }
     bitLenInt qubitsPerPage() { return log2(pageMaxQPower()); }
     int64_t GetPageDevice(bitCapIntOcl page) { return deviceIDs[page % deviceIDs.size()]; }
