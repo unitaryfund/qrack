7105b3ad71d542a4414e044a5a3b0ec23983ed8f
Daniel Strano <stranoj@gmail.com>
2022-08-25 12:05:30 -0400

    Tune PSTRIDEPOW

--- cmake/Pstridepow.cmake
+++ cmake/Pstridepow.cmake
@@ -1,5 +1,1 @@
-if (FPPOW EQUAL 4)
-    set(PSTRIDEPOW "10" CACHE STRING "Stride of parallel for loops (as a power of 2)")
-else (FPPOW EQUAL 4)
-    set(PSTRIDEPOW "9" CACHE STRING "Stride of parallel for loops (as a power of 2)")
-endif (FPPOW EQUAL 4)
+set(PSTRIDEPOW "10" CACHE STRING "Stride of parallel for loops (as a power of 2)")
