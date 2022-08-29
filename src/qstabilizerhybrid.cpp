c1edb7c25bb107b22e80fd2cdc2eea982766aa7a
Daniel Strano <stranoj@gmail.com>
2022-08-29 09:49:25 -0400

    Fix non-OCL builds

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -56,6 +56,8 @@ QStabilizerHybrid::QStabilizerHybrid(std::vector<QInterfaceEngine> eng, bitLenIn
 #if ENABLE_OPENCL
     DeviceContextPtr devContext = OCLEngine::Instance().GetDeviceContextPtr(devID);
     const bitLenInt maxPageQubits = log2(devContext->GetMaxAlloc() / sizeof(complex));
+#else
+    const bitLenInt maxPageQubits = getenv("QRACK_MAX_CPU_QB")) ? std::stoi(std::string(getenv("QRACK_MAX_CPU_QB"))) : 30U;
 #endif
 
 #if ENABLE_ENV_VARS
