ee6d0a5d349936752405747bdfc9d32ac0529869
Daniel Strano <stranoj@gmail.com>
2022-12-31 18:54:30 -0500

    Revert "Limit QBdt parallelism"
    This reverts commit 700986ed541fd80d62a42512c54de35bc4381032.

--- src/common/parallel_for.cpp
+++ src/common/parallel_for.cpp
@@ -209,7 +209,6 @@ void ParallelFor::par_for_inc(
     }
 }
 
-#if 0
 void ParallelFor::par_for_qbdt(const bitCapInt end, BdtFunc fn)
 {
     if (end < pStride) {
@@ -261,14 +260,6 @@ void ParallelFor::par_for_qbdt(const bitCapInt end, BdtFunc fn)
         futures[cpu].get();
     }
 }
-#else
-void ParallelFor::par_for_qbdt(const bitCapInt end, BdtFunc fn)
-{
-    for (bitCapInt j = 0U; j < end; ++j) {
-        j |= fn(j, 0U);
-    }
-}
-#endif
 
 real1_f ParallelFor::par_norm(const bitCapIntOcl itemCount, const StateVectorPtr stateArray, real1_f norm_thresh)
 {
