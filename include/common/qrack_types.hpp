d7f2988b8083d962239223a9460fc2b4a51967b9
Daniel Strano <stranoj@gmail.com>
2023-02-17 20:10:57 -0500

    Separate qrack_macros.hpp from qrack_types.hpp

--- include/common/qrack_types.hpp
+++ include/common/qrack_types.hpp
@@ -12,8 +12,8 @@
 
 #pragma once
 
-#define _USE_MATH_DEFINES
 #include "config.h"
+#include "qrack_macros.hpp"
 
 #include <complex>
 #include <functional>
@@ -22,92 +22,23 @@
 #include <set>
 #include <vector>
 
-#if UINTPOW < 4
-#define ONE_BCI ((uint8_t)1U)
-#define bitCapIntOcl uint8_t
-#elif UINTPOW < 5
-#define ONE_BCI ((uint16_t)1U)
-#define bitCapIntOcl uint16_t
-#elif UINTPOW < 6
-#define ONE_BCI 1U
-#define bitCapIntOcl uint32_t
-#else
-#define ONE_BCI 1UL
-#define bitCapIntOcl uint64_t
-#endif
-
-#if QBCAPPOW < 8
-#define bitLenInt uint8_t
-#elif QBCAPPOW < 16
-#define bitLenInt uint16_t
-#elif QBCAPPOW < 32
-#define bitLenInt uint32_t
-#else
-#define bitLenInt uint64_t
-#endif
-
-#if QBCAPPOW < 6
-#define bitsInCap 32
-#define bitCapInt uint32_t
-#elif QBCAPPOW < 7
-#define bitsInCap 64
-#define bitCapInt uint64_t
-#elif QBCAPPOW < 8
-#define bitsInCap 128
-#ifdef BOOST_AVAILABLE
+#if QBCAPPOW > 6 && defined(BOOST_AVAILABLE)
 #include <boost/multiprecision/cpp_int.hpp>
-#define bitCapInt boost::multiprecision::uint128_t
-#else
-#define bitCapInt __uint128_t
 #endif
-#else
-#define bitsInCap (8U * (((bitLenInt)1U) << QBCAPPOW))
-#include <boost/multiprecision/cpp_int.hpp>
-#define bitCapInt                                                                                                      \
-    boost::multiprecision::number<boost::multiprecision::cpp_int_backend<1ULL << QBCAPPOW, 1ULL << QBCAPPOW,           \
-        boost::multiprecision::unsigned_magnitude, boost::multiprecision::unchecked, void>>
-#endif
-
-#define bitsInByte 8U
-#define qrack_rand_gen std::mt19937_64
-#define qrack_rand_gen_ptr std::shared_ptr<qrack_rand_gen>
-#define QRACK_ALIGN_SIZE 64U
 
 #if FPPOW < 5
-#if !defined(__arm__)
-#include "half.hpp"
-#endif
 namespace Qrack {
 #ifdef __arm__
+#include "half.hpp"
 typedef std::complex<__fp16> complex;
 typedef __fp16 real1;
 typedef float real1_f;
 typedef float real1_s;
-#define ZERO_R1 0.0f
-#define ZERO_R1_F 0.0f
-#define ONE_R1 1.0f
-#define ONE_R1_F 1.0f
-#define PI_R1 ((real1_f)M_PI)
-#define SQRT2_R1 ((real1_f)M_SQRT2)
-#define SQRT1_2_R1 ((real1_f)M_SQRT2)
-#define REAL1_DEFAULT_ARG -999.0f
-// Half of the amplitude of 16 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 2e-17f
 #else
 typedef std::complex<half_float::half> complex;
 typedef half_float::half real1;
 typedef float real1_f;
 typedef float real1_s;
-#define ZERO_R1 ((real1)0.0f)
-#define ZERO_R1_F 0.0f
-#define ONE_R1 ((real1)1.0f)
-#define ONE_R1_F 1.0f
-#define PI_R1 ((real1)M_PI)
-#define SQRT2_R1 ((real1)M_SQRT2)
-#define SQRT1_2_R1 ((real1)M_SQRT1_2)
-#define REAL1_DEFAULT_ARG ((real1)-999.0f)
-// Half of the amplitude of 16 maximally superposed qubits in any permutation
-#define REAL1_EPSILON ((real1)2e-17f)
 #endif
 } // namespace Qrack
 #elif FPPOW < 6
@@ -116,16 +47,6 @@ typedef std::complex<float> complex;
 typedef float real1;
 typedef float real1_f;
 typedef float real1_s;
-#define ZERO_R1 0.0f
-#define ZERO_R1_F 0.0f
-#define ONE_R1 1.0f
-#define ONE_R1_F 1.0f
-#define PI_R1 ((real1_f)M_PI)
-#define SQRT2_R1 ((real1_f)M_SQRT2)
-#define SQRT1_2_R1 ((real1_f)M_SQRT1_2)
-#define REAL1_DEFAULT_ARG -999.0f
-// Half of the amplitude of 32 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 2e-33f
 } // namespace Qrack
 #elif FPPOW < 7
 namespace Qrack {
@@ -133,16 +54,6 @@ typedef std::complex<double> complex;
 typedef double real1;
 typedef double real1_f;
 typedef double real1_s;
-#define ZERO_R1 0.0
-#define ZERO_R1_F 0.0
-#define ONE_R1 1.0
-#define ONE_R1_F 1.0
-#define PI_R1 M_PI
-#define SQRT2_R1 M_SQRT2
-#define SQRT1_2_R1 M_SQRT1_2
-#define REAL1_DEFAULT_ARG -999.0
-// Half of the amplitude of 64 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 2e-65
 } // namespace Qrack
 #else
 #include <boost/multiprecision/float128.hpp>
@@ -152,27 +63,9 @@ typedef std::complex<boost::multiprecision::float128> complex;
 typedef boost::multiprecision::float128 real1;
 typedef boost::multiprecision::float128 real1_f;
 typedef double real1_s;
-#define ZERO_R1 ((real1)0.0)
-#define ZERO_R1_F 0.0
-#define ONE_R1 ((real1)1.0)
-#define ONE_R1_F 1.0
-#define PI_R1 ((real1)M_PI)
-#define SQRT2_R1 ((real1)M_SQRT2)
-#define SQRT1_2_R1 ((real1)M_SQRT1_2)
-#define REAL1_DEFAULT_ARG -999.0
-// Half of the amplitude of 64 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 2e-129
 // Minimum representable difference from 1
 } // namespace Qrack
 #endif
-
-#define ONE_CMPLX complex(ONE_R1, ZERO_R1)
-#define ZERO_CMPLX complex(ZERO_R1, ZERO_R1)
-#define I_CMPLX complex(ZERO_R1, ONE_R1)
-#define CMPLX_DEFAULT_ARG complex(REAL1_DEFAULT_ARG, REAL1_DEFAULT_ARG)
-#define FP_NORM_EPSILON std::numeric_limits<real1>::epsilon()
-#define FP_NORM_EPSILON_F ((real1_f)FP_NORM_EPSILON)
-#define TRYDECOMPOSE_EPSILON ((real1_f)(8 * FP_NORM_EPSILON))
 
 namespace Qrack {
 typedef std::shared_ptr<complex> BitOp;
