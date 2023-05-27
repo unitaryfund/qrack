b8c08f87f2707af60d28ca678fd5cdd5121629a0
Dan Strano <stranoj@gmail.com>
2023-05-27 12:20:20 -0400

    Prefer const auto&

--- include/qcircuit.hpp
+++ include/qcircuit.hpp
@@ -409,7 +409,7 @@ struct QCircuitGate {
                         pf |= pow2(i);
                     }
                 }
-                const auto poi = payloads.find(pf);
+                const auto& poi = payloads.find(pf);
                 if ((poi == payloads.end()) || (norm(poi->second.get()[0]) > FP_NORM_EPSILON)) {
                     nPayloads[payload.first] = payload.second;
                 } else {
@@ -438,7 +438,7 @@ struct QCircuitGate {
         complex identity[4] = { ONE_CMPLX, ZERO_CMPLX, ZERO_CMPLX, ONE_CMPLX };
         for (bitCapIntOcl i = 0U; i < maxQPower; ++i) {
             complex* mtrx = toRet.get() + (i << 2U);
-            const auto p = payloads.find(i);
+            const auto& p = payloads.find(i);
             if (p == payloads.end()) {
                 std::copy(identity, identity + 4, mtrx);
                 continue;
