e97a9dcc3167bbe0cebc4772c895ec9a73ed9fad
Dan Strano <stranoj@gmail.com>
2023-06-13 22:04:30 -0400

    Don't proactively flush phase buffers

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -857,24 +857,6 @@ void QStabilizerHybrid::Mtrx(const complex* lMtrx, bitLenInt target)
         return;
     }
 
-    if (useTGadget && (target < qubitCount) && ((qubitCount + ancillaCount) < maxQubitPlusAncillaCount) &&
-        IS_PHASE(mtrx)) {
-        QStabilizerPtr ancilla = std::make_shared<QStabilizer>(
-            1U, 0U, rand_generator, CMPLX_DEFAULT_ARG, false, randGlobalPhase, false, -1, useRDRAND);
-
-        // Form potentially entangled representation, with this.
-        bitLenInt ancillaIndex = stabilizer->Compose(ancilla);
-        ++ancillaCount;
-        shards.push_back(NULL);
-
-        // Use reverse t-injection gadget.
-        stabilizer->CNOT(target, ancillaIndex);
-        Mtrx(mtrx, ancillaIndex);
-        H(ancillaIndex);
-
-        return;
-    }
-
     shard = std::make_shared<MpsShard>(mtrx);
     if (!wasCached) {
         CacheEigenstate(target);
