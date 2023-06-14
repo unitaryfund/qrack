035d4d4abcb5045f844ebd07e1bbd47b7b4da077
Dan Strano <stranoj@gmail.com>
2023-06-13 20:08:37 -0400

    Reactively flush phase gates

--- src/qstabilizerhybrid.cpp
+++ src/qstabilizerhybrid.cpp
@@ -840,8 +840,26 @@ void QStabilizerHybrid::Mtrx(const complex* lMtrx, bitLenInt target)
     const bool wasCached = (bool)shard;
     complex mtrx[4U];
     if (wasCached) {
-        shard->Compose(lMtrx);
-        std::copy(shard->gate, shard->gate + 4U, mtrx);
+        if (useTGadget && (target < qubitCount) && ((qubitCount + ancillaCount) < maxQubitPlusAncillaCount) &&
+            IS_PHASE(shard->gate) && !IS_PHASE(lMtrx)) {
+            QStabilizerPtr ancilla = std::make_shared<QStabilizer>(
+                1U, 0U, rand_generator, CMPLX_DEFAULT_ARG, false, randGlobalPhase, false, -1, useRDRAND);
+
+            // Form potentially entangled representation, with this.
+            bitLenInt ancillaIndex = stabilizer->Compose(ancilla);
+            ++ancillaCount;
+            shards.push_back(NULL);
+
+            // Use reverse t-injection gadget.
+            stabilizer->CNOT(target, ancillaIndex);
+            Mtrx(shard->gate, ancillaIndex);
+            H(ancillaIndex);
+
+            std::copy(lMtrx, lMtrx + 4U, mtrx);
+        } else {
+            shard->Compose(lMtrx);
+            std::copy(shard->gate, shard->gate + 4U, mtrx);
+        }
         shard = NULL;
     } else {
         std::copy(lMtrx, lMtrx + 4U, mtrx);
@@ -854,24 +872,6 @@ void QStabilizerHybrid::Mtrx(const complex* lMtrx, bitLenInt target)
 
     if (IS_CLIFFORD(mtrx) || ((IS_PHASE(mtrx) || IS_INVERT(mtrx)) && stabilizer->IsSeparableZ(target))) {
         stabilizer->Mtrx(mtrx, target);
-        return;
-    }
-
-    if (useTGadget && (target < qubitCount) && ((qubitCount + ancillaCount) < maxQubitPlusAncillaCount) &&
-        IS_PHASE(mtrx)) {
-        QStabilizerPtr ancilla = std::make_shared<QStabilizer>(
-            1U, 0U, rand_generator, CMPLX_DEFAULT_ARG, false, randGlobalPhase, false, -1, useRDRAND);
-
-        // Form potentially entangled representation, with this.
-        bitLenInt ancillaIndex = stabilizer->Compose(ancilla);
-        ++ancillaCount;
-        shards.push_back(NULL);
-
-        // Use reverse t-injection gadget.
-        stabilizer->CNOT(target, ancillaIndex);
-        Mtrx(mtrx, ancillaIndex);
-        H(ancillaIndex);
-
         return;
     }
 
