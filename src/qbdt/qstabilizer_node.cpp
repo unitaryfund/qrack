72a73acc5fc78c57259b5730b4ec9e4e05f0c151
Dan Strano <stranoj@gmail.com>
2023-08-05 16:57:45 -0400

    Debug PopSpecial()

--- src/qbdt/qstabilizer_node.cpp
+++ src/qbdt/qstabilizer_node.cpp
@@ -160,55 +160,66 @@ QBdtNodeInterfacePtr QBdtQStabilizerNode::PopSpecial(bitLenInt depth)
     QBdtNodeInterfacePtr nRoot = std::make_shared<QBdtNode>(scale);
 
     // We need a Bell pair for teleportation, with one end on each side of the QBDT/stabilizer domain wall.
-    // We allocate one end in the stabilizer simulator.
     const bitLenInt aliceBellBit = qReg->GetQubitCount();
-    qReg->Allocate(1U);
-
     // Creating a "new root" (to replace keyword "this" class instance node, on return) effectively allocates a new
     // qubit reset to |+>, (or effectively |0> followed by H gate).
-    const QStabilizerPtr qRegB1 = std::dynamic_pointer_cast<QStabilizer>(qReg->Clone());
-    nRoot->branches[0U] = std::make_shared<QBdtQStabilizerNode>(SQRT1_2_R1, qReg);
-    nRoot->branches[1U] = std::make_shared<QBdtQStabilizerNode>(SQRT1_2_R1, qRegB1);
-    QBdtNodeInterfacePtr& b0 = nRoot->branches[0U];
-    QBdtNodeInterfacePtr& b1 = nRoot->branches[1U];
+    QBdtNodeInterfacePtr& b0 = nRoot->branches[0U] =
+        std::make_shared<QBdtQStabilizerNode>(SQRT1_2_R1, std::dynamic_pointer_cast<QStabilizer>(qReg->Clone()));
+    QBdtNodeInterfacePtr& b1 = nRoot->branches[1U] =
+        std::make_shared<QBdtQStabilizerNode>(SQRT1_2_R1, std::dynamic_pointer_cast<QStabilizer>(qReg->Clone()));
+
+    QStabilizerPtr qReg0 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b0)->qReg;
+    QStabilizerPtr qReg1 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b1)->qReg;
+    // We allocate the other Bell pair end in the stabilizer simulator.
+    qReg0->Allocate(1U);
+    qReg1->Allocate(1U);
 
     // We act CNOT from |+> control to |0> target.
     // (Notice, we act X gate in |1> branch and no gate in |0> branch.)
-    qRegB1->X(aliceBellBit);
+    qReg1->X(aliceBellBit);
     nRoot->Prune(2U);
     // This is the Bell pair "Eve" creates to distribute to "Alice" and "Bob," in quantum teleportation.
+
+    qReg0 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b0)->qReg;
+    qReg1 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b1)->qReg;
 
     // "Alice" prepares and sends a qubit to "Bob"; stabilizer prepares and sends a qubit to QBDT.
     // Alice's "prepared state" to teleport is the 0U index stabilizer qubit, (same in both branches).
     // Alice uses the "prepared state" qubit as the control of a CNOT on the Bell pair.
-    qReg->CNOT(0U, aliceBellBit);
-    qRegB1->CNOT(0U, aliceBellBit);
+    qReg0->CNOT(0U, aliceBellBit);
+    qReg1->CNOT(0U, aliceBellBit);
     nRoot->Prune(2U);
 
+    qReg0 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b0)->qReg;
+    qReg1 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b1)->qReg;
+
     // Alice acts H on her "prepared state":
-    qReg->H(0U);
-    qRegB1->H(0U);
+    qReg0->H(0U);
+    qReg1->H(0U);
     nRoot->Prune(2U);
+
+    qReg0 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b0)->qReg;
+    qReg1 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b1)->qReg;
 
     // Alice now measures both of her bits, and records the results.
 
     // First, measure Alice's Bell pair bit.
-    const real1 p01 = qReg->Prob(aliceBellBit);
-    const real1 p11 = qRegB1->Prob(aliceBellBit);
+    const real1 p01 = qReg0->Prob(aliceBellBit);
+    const real1 p11 = qReg1->Prob(aliceBellBit);
     const bool q1 = qReg->Rand() < ((p01 + p11) / 2);
 
     bool isB0 = !((q1 && IS_0_PROB(p01)) || (!q1 && IS_1_PROB(p01)));
     if (isB0) {
-        qReg->ForceM(aliceBellBit, q1);
-        qReg->Dispose(aliceBellBit, 1U);
+        qReg0->ForceM(aliceBellBit, q1);
+        qReg0->Dispose(aliceBellBit, 1U);
     } else {
         b0->SetZero();
     }
 
     bool isB1 = !((q1 && IS_0_PROB(p11)) || (!q1 && IS_1_PROB(p11)));
     if (isB1) {
-        qRegB1->ForceM(aliceBellBit, q1);
-        qRegB1->Dispose(aliceBellBit, 1U);
+        qReg1->ForceM(aliceBellBit, q1);
+        qReg1->Dispose(aliceBellBit, 1U);
     } else {
         b1->SetZero();
     }
@@ -216,17 +227,20 @@ QBdtNodeInterfacePtr QBdtQStabilizerNode::PopSpecial(bitLenInt depth)
     nRoot->Prune(2U);
     nRoot->Normalize(2U);
 
+    qReg0 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b0)->qReg;
+    qReg1 = std::dynamic_pointer_cast<QBdtQStabilizerNode>(b1)->qReg;
+
     // Next, measure Alice's "prepared state" bit.
-    const real1 p00 = isB0 ? qReg->Prob(0U) : qRegB1->Prob(0U);
-    const real1 p10 = isB1 ? qRegB1->Prob(0U) : qReg->Prob(0U);
+    const real1 p00 = isB0 ? qReg0->Prob(0U) : qReg1->Prob(0U);
+    const real1 p10 = isB1 ? qReg1->Prob(0U) : qReg0->Prob(0U);
     const bool q0 = qReg->Rand() < ((p00 + p10) / 2);
 
     if (isB0) {
         if ((q0 && IS_0_PROB(p00)) || (!q0 && IS_1_PROB(p00))) {
             b0->SetZero();
         } else {
-            qReg->ForceM(0U, q0);
-            qReg->Dispose(0U, 1U);
+            qReg0->ForceM(0U, q0);
+            qReg0->Dispose(0U, 1U);
         }
     }
 
@@ -234,8 +248,8 @@ QBdtNodeInterfacePtr QBdtQStabilizerNode::PopSpecial(bitLenInt depth)
         if ((q0 && IS_0_PROB(p10)) || (!q0 && IS_1_PROB(p10))) {
             b1->SetZero();
         } else {
-            qRegB1->ForceM(0U, q0);
-            qRegB1->Dispose(0U, 1U);
+            qReg1->ForceM(0U, q0);
+            qReg1->Dispose(0U, 1U);
         }
     }
 
@@ -245,11 +259,11 @@ QBdtNodeInterfacePtr QBdtQStabilizerNode::PopSpecial(bitLenInt depth)
     // Bob acts 0 to 2 corrective gates based upon Alice's measured bits.
     if (q0) {
         b1->scale = -b1->scale;
-        nRoot->Prune(2U);
+        nRoot->Prune();
     }
     if (q1) {
         std::swap(b0, b1);
-        nRoot->Prune(2U);
+        nRoot->Prune();
     }
 
     // This process might need to be repeated, recursively.
@@ -259,7 +273,7 @@ QBdtNodeInterfacePtr QBdtQStabilizerNode::PopSpecial(bitLenInt depth)
     if (!IS_NORM_0(b1->scale)) {
         b1 = b1->PopSpecial(depth);
     }
-    nRoot->Prune(2U);
+    nRoot->Prune();
 
     // We're done! Just return the replacement for "this" pointer.
     return nRoot;
