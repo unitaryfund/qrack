e4a967ce229cd394e4b77abcc2ac322cae2934e9
Dan Strano <stranoj@gmail.com>
2023-11-02 09:02:00 -0400

    Fix qbdt_hybrid.cpp header

--- src/qbdthybrid.cpp
+++ src/qbdthybrid.cpp
@@ -2,7 +2,9 @@
 //
 // (C) Daniel Strano and the Qrack contributors 2017-2023. All rights reserved.
 //
-// QPager breaks a QEngine instance into pages of contiguous amplitudes.
+// This is a multithreaded, universal quantum register simulation, allowing
+// (nonphysical) register cloning and direct measurement of probability and
+// phase, to leverage what advantages classical emulation of qubits can have.
 //
 // Licensed under the GNU Lesser General Public License V3.
 // See LICENSE.md in the project root or https://www.gnu.org/licenses/lgpl-3.0.en.html
