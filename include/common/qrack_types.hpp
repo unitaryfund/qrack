4d966fb49339b2161ff039d7d7adafb33557d281
Daniel Strano <stranoj@gmail.com>
2024-01-05 18:54:57 -0500

    real1 limits follow from talk with @nelimee

--- include/common/qrack_types.hpp
+++ include/common/qrack_types.hpp
@@ -159,8 +159,8 @@ constexpr real1_f ZERO_R1_F = 0.0f;
 const real1 ONE_R1 = (real1)1.0f;
 constexpr real1_f ONE_R1_F = 1.0f;
 const real1 REAL1_DEFAULT_ARG = (real1)-999.0f;
-// Half of the probability of 16 maximally superposed qubits in any permutation
-const real1 REAL1_EPSILON = (real1)0.00000762939f;
+// Half of the probability of 20 maximally superposed qubits in any permutation
+const real1 REAL1_EPSILON = (real1)0.000000477f;
 const real1 PI_R1 = (real1)M_PI;
 const real1 SQRT2_R1 = (real1)M_SQRT2;
 const real1 SQRT1_2_R1 = (real1)M_SQRT1_2;
@@ -174,8 +174,8 @@ constexpr real1 PI_R1 = (real1)M_PI;
 constexpr real1 SQRT2_R1 = (real1)M_SQRT2;
 constexpr real1 SQRT1_2_R1 = (real1)M_SQRT1_2;
 #define REAL1_DEFAULT_ARG -999.0f
-// Half of the probability of 32 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 1.1641532e-10
+// Half of the probability of 48 maximally superposed qubits in any permutation
+#define REAL1_EPSILON 1.7763568394002505e-15f
 #elif FPPOW < 7
 #define QRACK_CONST constexpr
 #define ZERO_R1 0.0
@@ -186,8 +186,8 @@ constexpr real1 SQRT1_2_R1 = (real1)M_SQRT1_2;
 #define SQRT2_R1 M_SQRT2
 #define SQRT1_2_R1 M_SQRT1_2
 #define REAL1_DEFAULT_ARG -999.0
-// Half of the probability of 64 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 2.7105054e-20
+// Half of the probability of 96 maximally superposed qubits in any permutation
+#define REAL1_EPSILON 1.262177448353619e-29
 #else
 #define QRACK_CONST constexpr
 constexpr real1 ZERO_R1 = (real1)0.0;
@@ -198,8 +198,8 @@ constexpr real1_f PI_R1 = (real1_f)M_PI;
 constexpr real1_f SQRT2_R1 = (real1_f)M_SQRT2;
 constexpr real1_f SQRT1_2_R1 = (real1_f)M_SQRT1_2;
 #define REAL1_DEFAULT_ARG -999.0
-// Half of the probability of 64 maximally superposed qubits in any permutation
-#define REAL1_EPSILON 1.4693679e-39
+// Half of the probability of 192 maximally superposed qubits in any permutation
+#define REAL1_EPSILON 1.5930919111324523e-58
 #endif
 
 #if ENABLE_CUDA
