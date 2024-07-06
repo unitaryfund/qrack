32f0bf49b0498005d7790b88422333710b6be9c3
WrathfulSpatula <stranoj@gmail.com>
2024-07-06 08:06:40 -0400

    Standards/style

--- src/qinterface/qinterface.cpp
+++ src/qinterface/qinterface.cpp
@@ -83,7 +83,7 @@ QInterface::QInterface(
         }
 #else
         std::random_device rd;
-        std::uniform_int_distribution<uint32_t> dist(0, pow2Ocl(32U) - 1U);
+        std::uniform_int_distribution<uint32_t> dist(0U, pow2Ocl(32U) - 1U);
         randomSeed = dist(rd);
 #endif
         SetRandomSeed(randomSeed);
