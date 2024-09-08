66c0050cc9edbe379591d96716f5ade832b514a8
WrathfulSpatula <stranoj@gmail.com>
2024-09-08 14:25:40 -0400

    Minor style change

--- src/qtensornetwork.cpp
+++ src/qtensornetwork.cpp
@@ -230,7 +230,7 @@ bool QTensorNetwork::ForceM(bitLenInt qubit, bool result, bool doForce, bool doA
     }
 
     // Identify whether we need a totally new measurement layer.
-    if ((layerId + 1U) > measurements.size()) {
+    if (layerId >= measurements.size()) {
         // Insert the required measurement layer.
         measurements.emplace_back();
     }
