4ca3ec5bc23c4d0eee8070fb60df87014b0fe98a
WrathfulSpatula <stranoj@gmail.com>
2024-09-23 14:21:31 -0400

    Fix CMake CppStd

--- cmake/CppStd.cmake
+++ cmake/CppStd.cmake
@@ -1,1 +1,1 @@
-option (CPP_STD "C++ standard to use (options are '11', '14', '17', '20', and '23')" 14)
+set (CPP_STD 14 CACHE STRING "C++ standard to use (options are '11', '14', '17', '20', and '23')")
