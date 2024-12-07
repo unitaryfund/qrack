099db14bcee504b3f8400f910b763192a0ea6d3c
WrathfulSpatula <stranoj@gmail.com>
2024-12-07 11:26:09 -0500

    Improve PSTRIDEPOW

--- cmake/Pstridepow.cmake
+++ cmake/Pstridepow.cmake
@@ -1,2 +1,2 @@
-math(EXPR ADJ_PSTRIDEPOW "16 - ${FPPOW}")
+math(EXPR ADJ_PSTRIDEPOW "17 - ${FPPOW}")
 set(PSTRIDEPOW "${ADJ_PSTRIDEPOW}" CACHE STRING "Stride of parallel for loops (as a power of 2)")
